de923d710af390768e4c7756ca97e9617891735c
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2016-11-25 00:39:29 -0500

    Test main()'s output formats

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,9 +1,11 @@
 import datetime
 import json
 import os.path
+import sys
 import unittest
 
 from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from clippings.parser import Clipping
 from clippings.parser import Document
@@ -12,7 +14,11 @@ from clippings.parser import Metadata
 from clippings.parser import as_dicts
 from clippings.parser import as_json
 from clippings.parser import as_kindle
+from clippings.parser import main as parser_main
 from clippings.parser import parse_clippings
+
+from .utils.context import cli_args
+from .utils.context import capture_stdout
 
 
 class DefaultObjectFactoryMixin:
@@ -341,3 +347,46 @@ class ClippingFileParsingTest(unittest.TestCase):
 
         self.assertIsNotNone(clippings)
         self.assertEqual(5, len(clippings), '5 clippings should be parsed!')
+
+
+class MainFunctionTest(unittest.TestCase):
+
+    def test_output_format_json(self):
+        with cli_args(['tests/resources/clippings.txt', '-o', 'json']), \
+             patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
+             capture_stdout() as stdout:
+
+            parser_main()
+
+        self.assertTrue(as_json_mock.called)
+        self.assertEqual('{"j": "son"}', stdout.read())
+
+    def test_output_format_dict(self):
+        with cli_args(['tests/resources/clippings.txt', '-o', 'dict']), \
+             patch('clippings.parser.as_dicts', return_value={'d': 'ict'}) as as_dicts_mock, \
+             capture_stdout() as stdout:
+
+            parser_main()
+
+        self.assertTrue(as_dicts_mock.called)
+        self.assertEqual(str({'d': 'ict'}), stdout.read())
+
+    def test_output_format_kindle(self):
+        with cli_args(['tests/resources/clippings.txt', '-o', 'kindle']), \
+             patch('clippings.parser.as_kindle', return_value='kindle') as as_kindle_mock, \
+             capture_stdout() as stdout:
+
+            parser_main()
+
+        self.assertTrue(as_kindle_mock.called)
+        self.assertEqual('kindle', stdout.read())
+
+    def test_output_format_defaults_to_json(self):
+        with cli_args(['tests/resources/clippings.txt']), \
+             patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
+             capture_stdout() as stdout:
+
+            parser_main()
+
+        self.assertTrue(as_json_mock.called)
+        self.assertEqual('{"j": "son"}', stdout.read())
