cd66141764b2aceed4636f073e9283f2f50fcaa8
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2021-01-12 23:01:41 -0500

    Convert CLI tests to pytest-style

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,4 +1,3 @@
-import unittest
 from unittest import mock
 
 from clippings.parser import main as parser_main
@@ -7,44 +6,45 @@ from .utils.context import cli_args
 from .utils.context import capture_stdout
 
 
-class MainFunctionTest(unittest.TestCase):
+def test_output_format_json():
+    with cli_args(['tests/resources/clippings.txt', '-o', 'json']), \
+         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
+         capture_stdout() as stdout:
 
-    def test_output_format_json(self):
-        with cli_args(['tests/resources/clippings.txt', '-o', 'json']), \
-             mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
-             capture_stdout() as stdout:
+        parser_main()
 
-            parser_main()
+    as_json_mock.assert_called_once()
+    assert stdout.read() == '{"j": "son"}'
 
-        self.assertTrue(as_json_mock.called)
-        self.assertEqual('{"j": "son"}', stdout.read())
 
-    def test_output_format_dict(self):
-        with cli_args(['tests/resources/clippings.txt', '-o', 'dict']), \
-             mock.patch('clippings.parser.as_dicts', return_value={'d': 'ict'}) as as_dicts_mock, \
-             capture_stdout() as stdout:
+def test_output_format_dict():
+    with cli_args(['tests/resources/clippings.txt', '-o', 'dict']), \
+         mock.patch('clippings.parser.as_dicts', return_value={'d': 'ict'}) as as_dicts_mock, \
+         capture_stdout() as stdout:
 
-            parser_main()
+        parser_main()
 
-        self.assertTrue(as_dicts_mock.called)
-        self.assertEqual(str({'d': 'ict'}), stdout.read())
+    as_dicts_mock.assert_called_once()
+    assert stdout.read() == str({'d': 'ict'})
 
-    def test_output_format_kindle(self):
-        with cli_args(['tests/resources/clippings.txt', '-o', 'kindle']), \
-             mock.patch('clippings.parser.as_kindle', return_value='kindle') as as_kindle_mock, \
-             capture_stdout() as stdout:
 
-            parser_main()
+def test_output_format_kindle():
+    with cli_args(['tests/resources/clippings.txt', '-o', 'kindle']), \
+         mock.patch('clippings.parser.as_kindle', return_value='kindle') as as_kindle_mock, \
+         capture_stdout() as stdout:
 
-        self.assertTrue(as_kindle_mock.called)
-        self.assertEqual('kindle', stdout.read())
+        parser_main()
 
-    def test_output_format_defaults_to_json(self):
-        with cli_args(['tests/resources/clippings.txt']), \
-             mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
-             capture_stdout() as stdout:
+    as_kindle_mock.assert_called_once()
+    assert stdout.read() == 'kindle'
 
-            parser_main()
 
-        self.assertTrue(as_json_mock.called)
-        self.assertEqual('{"j": "son"}', stdout.read())
+def test_output_format_defaults_to_json():
+    with cli_args(['tests/resources/clippings.txt']), \
+         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
+         capture_stdout() as stdout:
+
+        parser_main()
+
+    as_json_mock.assert_called_once()
+    assert stdout.read() == '{"j": "son"}'
