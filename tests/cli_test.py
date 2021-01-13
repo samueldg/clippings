cdbc966aca12b04d3008849034704c209be1273f
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2021-01-12 23:04:12 -0500

    Replace homemade sys.stdout capture tool with pytest's

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -3,48 +3,43 @@ from unittest import mock
 from clippings.parser import main as parser_main
 
 from .utils.context import cli_args
-from .utils.context import capture_stdout
 
 
-def test_output_format_json():
+def test_output_format_json(capsys):
     with cli_args(['tests/resources/clippings.txt', '-o', 'json']), \
-         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
-         capture_stdout() as stdout:
+         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock:
 
         parser_main()
 
     as_json_mock.assert_called_once()
-    assert stdout.read() == '{"j": "son"}'
+    assert capsys.readouterr().out == '{"j": "son"}'
 
 
-def test_output_format_dict():
+def test_output_format_dict(capsys):
     with cli_args(['tests/resources/clippings.txt', '-o', 'dict']), \
-         mock.patch('clippings.parser.as_dicts', return_value={'d': 'ict'}) as as_dicts_mock, \
-         capture_stdout() as stdout:
+         mock.patch('clippings.parser.as_dicts', return_value={'d': 'ict'}) as as_dicts_mock:
 
         parser_main()
 
     as_dicts_mock.assert_called_once()
-    assert stdout.read() == str({'d': 'ict'})
+    assert capsys.readouterr().out == str({'d': 'ict'})
 
 
-def test_output_format_kindle():
+def test_output_format_kindle(capsys):
     with cli_args(['tests/resources/clippings.txt', '-o', 'kindle']), \
-         mock.patch('clippings.parser.as_kindle', return_value='kindle') as as_kindle_mock, \
-         capture_stdout() as stdout:
+         mock.patch('clippings.parser.as_kindle', return_value='kindle') as as_kindle_mock:
 
         parser_main()
 
     as_kindle_mock.assert_called_once()
-    assert stdout.read() == 'kindle'
+    assert capsys.readouterr().out == 'kindle'
 
 
-def test_output_format_defaults_to_json():
+def test_output_format_defaults_to_json(capsys):
     with cli_args(['tests/resources/clippings.txt']), \
-         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock, \
-         capture_stdout() as stdout:
+         mock.patch('clippings.parser.as_json', return_value='{"j": "son"}') as as_json_mock:
 
         parser_main()
 
     as_json_mock.assert_called_once()
-    assert stdout.read() == '{"j": "son"}'
+    assert capsys.readouterr().out == '{"j": "son"}'
--- tests/utils/context.py
+++ tests/utils/context.py
@@ -1,6 +1,5 @@
 import sys
 from contextlib import contextmanager
-from io import StringIO
 from unittest.mock import patch
 
 
@@ -17,20 +16,3 @@ def cli_args(arg_list):
     mock_argv = ['clippings.py'] + arg_list
     with patch.object(sys, 'argv', mock_argv):
         yield
-
-
-@contextmanager
-def capture_stdout():
-    """Context manager to intercept data written to stdout.
-
-    After the context exits, the original stdout is restored,
-    and the fake stdout can be read like a file.
-    """
-    stdout = sys.stdout
-    stdout_capture = StringIO()
-    sys.stdout = stdout_capture
-
-    yield stdout_capture
-
-    sys.stdout = stdout
-    stdout_capture.seek(0)
